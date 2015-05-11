8c899b3731b92e083d7ed931062feaa28b1a137d
Colter Williams <cjwilliams@fullsail.edu>
2015-05-11 16:25:38 -0700

    started of problem 1
    kinda stuck on problem 1. but almost done

--- Expressions_worksheet/js folder/script.js1.js
+++ Expressions_worksheet/js folder/script.js1.js
@@ -1,5 +1,30 @@
 // Colter Williams 5/11/15 Expression Worksheet
 
-alert("1, 2 ,3");
+//create variables for sparkys age and sparkys human age
+//Given: Sparkys age
+var sparkysAge = 1;
+//Result variable: Sparkys age in dog years.
+var dogyearsAge = 7;
+// now create a string that tells what sparkys age is in human years and dog years.
+var result = String("Sparky is " + sparkysAge) + String(" human years old which is " + dogyearsAge) + String(" in dog years. ");
+//make a console log to check result.
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
