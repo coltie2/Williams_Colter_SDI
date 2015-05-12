74e248fac2de750ccb88d7bd9d460dbff5149666
Colter Williams <cjwilliams@fullsail.edu>
2015-05-12 09:38:43 -0700

    Finished prob 1,2
    Finished problem 1 and 2. I’m happy I figured them out!

--- Expressions_worksheet/js folder/script.js1.js
+++ Expressions_worksheet/js folder/script.js1.js
@@ -1,14 +1,42 @@
 // Colter Williams 5/11/15 Expression Worksheet
 
-//create variables for sparkys age and sparkys human age
-//Given: Sparkys age
-var sparkysAge = 1;
-//Result variable: Sparkys age in dog years.
-var dogyearsAge = 7;
-// now create a string that tells what sparkys age is in human years and dog years.
-var result = String("Sparky is " + sparkysAge) + String(" human years old which is " + dogyearsAge) + String(" in dog years. ");
-//make a console log to check result.
-console.log(result);
+//Create variables for Sparky's human age and set it equal to any number.
+var humanAge = 2;
+
+//Create variable for dog years which is human age * 7
+var dogAge = humanAge * 7;
+
+//Print out the output using concatenation for the variables.
+console.log("Sparky is " +humanAge+ " human years old which is " +dogAge+ " in dog years.");
+
+//PROBLEM 2
+
+//Create variables for number of slices per pizza.
+var slicesperPizza = 8;
+
+//Create variable for how many people are at the party.
+var numberofPeople = 10;
+
+//Create variable for number of pizzas ordered.
+var pizzaOrdered = 5;
+
+//Create variable "r" to = pizzaOrdered * slicesperPizza.
+var r = pizzaOrdered * slicesperPizza;
+
+//Create variable "p" to = r / numberofPeople.
+var p = r / numberofPeople;
+
+//Print out the output using concatenation for the variable.
+console.log("Each person ate " +p+ " slices of pizza at the party.");
+
+
+//PROBLEM 3
+
+
+
+
+
+
 
 
 
