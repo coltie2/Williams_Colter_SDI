886c29276a74712771a8ec7ebdb7d048bed05567
Colter Williams <cjwilliams@fullsail.edu>
2015-05-14 12:51:16 -0700

    got the full calculator going
    created my problem, got the calc working, just need to make some
    changes!

--- Williams_Colter_Expressions_Assignment/js/script.js
+++ Williams_Colter_Expressions_Assignment/js/script.js
@@ -11,18 +11,40 @@ alert("Welcome to the cement calculator");
 alert("This calculator will help you determine the cubic feet needed to complete your project and convert the cubic feet to cubic yards when ordering your cement.");
 
 //Create variables for width, height, and depth.
-var width = prompt("Please enter your width");
+var width = prompt("Please enter your width:");
 console.log(width);
-var height = prompt("Please enter your height");
+var height = prompt("Please enter your height:");
 console.log(height);
-var depth = prompt("Please enter your depth");
+var depth = prompt("Please enter your depth:");
 console.log(depth);
 
 //Add loading screen joke
-alert("Beep..Boop..Boop..Loading answer");
+alert("Beep..Boop..Boop..Loading answer!");
 
 //Create a array using the variable above an label it font dimensions.
 fontDimensions = [width,height,depth]; // 0,1,2
+
+//Create an alert and times the 3 parts of the array together to get how many cubic feet you have.
+ alert("You have " +fontDimensions[0] * +fontDimensions[1] * +fontDimensions[2] +" cubic feet!");
+
+//Cement is ordered by the cubic YARD so we must now convert cubic "feet" to cubic YARDS.
+// 1 cubic yard is 27 cubic feet
+alert("Since cement is ordered by the cubic yard, this calculator will now give you how many cubic yards you will need");
+
+//Create variable for 27 cubic feet/
+var endResult = 27;
+
+//Devide the total amount of cubic feet entered by 27 cubic feet.
+alert("You will need " +fontDimensions[0] * +fontDimensions[1] * +fontDimensions[2] / +endResult +" cubic yards");
+
+
+
+
+
+
+
+
+
 
 
 
