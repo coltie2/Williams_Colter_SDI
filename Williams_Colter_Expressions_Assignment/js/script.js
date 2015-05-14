ba8b17e0892d7e1b9a1d9e92577f1e8ef0232fc6
Colter Williams <cjwilliams@fullsail.edu>
2015-05-14 12:15:16 -0700

    Created Williams_Colter_Expressions_Assignment
    created my assignment and finished the intro

---
+++ Williams_Colter_Expressions_Assignment/js/script.js
@@ -0,0 +1,28 @@
+/*
+Colter Williams
+5-13-15
+Section 01
+Williams_Colter_Expressions_Assigment.
+ */
+
+//Create welcome alert.
+alert("Welcome to the cement calculator");
+//Create an alert explaining what this calculator will do.
+alert("This calculator will help you determine the cubic feet needed to complete your project and convert the cubic feet to cubic yards when ordering your cement.");
+
+//Create variables for width, height, and depth.
+var width = prompt("Please enter your width");
+console.log(width);
+var height = prompt("Please enter your height");
+console.log(height);
+var depth = prompt("Please enter your depth");
+console.log(depth);
+
+//Create a array using the variable above an label it font dimensions.
+fontDimensions = [width,height,depth]; // 0,1,2
+
+
+
+
+
+
