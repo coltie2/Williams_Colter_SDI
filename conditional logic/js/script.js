bec4d6a824956dcf6c893d3bd33d81c4e7a59953
Colter Williams <cjwilliams@fullsail.edu>
2015-05-17 11:29:12 -0700

    finished notes
    added turneries

--- conditional logic/js/script.js
+++ conditional logic/js/script.js
@@ -4,7 +4,7 @@
 
 //alert("pigs feet");
 //conditional logic
-var kidHeight = 52;
+/*var kidHeight = 52;
 var minHeight = 48;
 var parentHeight = 45; //the height of the kid with parent
 //if the child is old enough, print to the console "you can ride".
@@ -20,9 +20,22 @@ if(kidHeight > minHeight){
      //sorry you have growing to do
     console.log("sorry kid you have some growing to do");
 }
+*/
 
+/*var budget = 100;
+var iphonePrice= 199.99;
+var wonLottery = true;
+//if the price of the phone is less our budge
+if(iphonePrice < budget || wonLottery){
+  console.log("we can buy the phone!");
+}else{
+  console.log("no phone for you!");
+} */
 
-
-
-
+var age= 11;
+var book;
+//if the child is under 10, they get green age and ham, if not they get time machie.
+book = (age <10) ? "green eggs and ham" : "the time machine";
+console.log(book;
+)
 
