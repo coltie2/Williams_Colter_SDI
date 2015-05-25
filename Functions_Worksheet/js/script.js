ea30f5aacb6b171969f047576a27b26265945c1e
Colter Williams <cjwilliams@fullsail.edu>
2015-05-25 14:55:11 -0700

    created worksheet
    created worksheet and made a var for diameter

---
+++ Functions_Worksheet/js/script.js
@@ -0,0 +1,9 @@
+/*/**
+ * Created by ColterWilliams on 5/25/15.
+ * Functions_Worksheet
+ * Section 01
+ */
+
+//Calculate the circumference of a circle@
+//Create a variable for diameter
+var diameter = 2;
