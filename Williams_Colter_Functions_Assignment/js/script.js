26b57067f5ac85350c2502fb93f30c05a0020e67
Colter Williams <cjwilliams@fullsail.edu>
2015-05-26 14:27:30 -0700

    Final changes made
    Ready to turn in, final changes have been made!

--- Williams_Colter_Functions_Assignment/js/script.js
+++ Williams_Colter_Functions_Assignment/js/script.js
@@ -9,13 +9,16 @@
 //created test to see if html is connected
 //alert('testing 1,2,3');
 
+//create hello
+alert("Loading the worlds finest body weight calculator!");
+
 //create welcome calculator alert
-alert("Welcome to the body calculator. This calculator will help you calculate how many pounds you have added/subtracted and then tell you what that percentage is. ");
+alert("Welcome to the BodMaster. This calculator will help you calculate how many pounds you have gained/lose and then tell you what that percentage of weight change is. ");
 
 //create variable for old weight (pounds)
-var oldWeight = prompt("Enter old weight in pounds");
+var oldWeight = prompt("Enter your old weight in pounds.");
 
-//valiate for BOTH blank and a number using a while loop
+//validate for BOTH blank and a number using a while loop
 while (oldWeight==="" || isNaN(oldWeight)){
 
     oldWeight = prompt("please do not leave blank and only use numbers.\nType in a number.");
@@ -23,34 +26,36 @@ while (oldWeight==="" || isNaN(oldWeight)){
 }
 
 //create variable for current weight (pounds)
-var currentWeight = prompt("Enter current weight in pounds");
+var currentWeight = prompt("Enter your current weight in pounds.");
 
-//valiate for BOTH blank and a number using a while loop
+//validate for BOTH blank and a number using a while loop
 while (currentWeight==="" || isNaN(currentWeight)) {
 
     currentWeight = prompt("please do not leave blank and only use numbers.\nType in a number.");
 }
 
-//percent is 100 variable
-var percent100 = 100;
+//create percent is 100 variable
+var percent100 = 100; //this WILL be used to turn weight change into a percent of weight change.
 
 //loading screen for fun
 alert("Processing information! One moment Please.");
 
 //create function variable for weight
-var resultWeight = calculatePercent(currentWeight,oldWeight);
+var resultWeight = calculateWeight(currentWeight,oldWeight);
 
+//send info to the console
 console.log("Your weight change is " +resultWeight +" pounds.");
 
+//alert the user what their weight change is in pounds
 alert("Your weight change is " +resultWeight +" pounds.");
 
 //create first function
-function calculatePercent(c,o){
+function calculateWeight(c,o){
       return c - o;
 }
 
-//prompt the user to input the weight number given
-var weight = prompt("Enter the amount of wait change the calculator just gave to you. Now the calculator will tell you your percent of weight change.");
+//prompt the user to input the weightchange number that was given to them
+var weight = prompt("Enter the weight change the calculator just gave to you. Now the calculator will tell you your percent of weight change.");
 
 //valiate for BOTH blank and a number using a while loop
 while (weight==="" || isNaN(weight)) {
@@ -58,11 +63,28 @@ while (weight==="" || isNaN(weight)) {
    weight = prompt("please do not leave blank and only use numbers.\nType in a number.");
 }
 
+//loading screen for fun
+alert("Processing information! One moment Please.");
 
-var resultPercent = function(w,ow,p){
-    var result = w / ow * p;
+//create variable to use in the anonymous function//second function with 3 parameters.
+var resultPercent = function(w,ow,p){ //3 parameters
+    var result = w / ow * p; //code the function runs//basically this is the amount of weight received from the function above / old weight * 100 to be given the percent of your weight change.
     return result;
-
-}
+};
+//create variable a = resultPercent
 var a = resultPercent(weight,oldWeight,percent100);
-console.log(a);
+
+//send info to the console (weight change percent)
+console.log("Your percent of weight change is " +a +"%.");
+
+//alert to show the user their final result
+alert("Your percent of weight change is " +a +"%.");
+
+//create goodbye alert
+alert("Thank you for using the BodMaster, please come again soon!");
+
+/* So to test my calculator I typed in 150 as my old weight, and 155 as my current weight.
+The calculator said i've gained 5 pounds. Next the calculator asked me to type in the weight change it gave me.
+So I typed in 5 and it turned that 5 pounds into a percentage of weight gained which was 3.33333%.
+The calculator 100% works:) woohoooo.
+ */
