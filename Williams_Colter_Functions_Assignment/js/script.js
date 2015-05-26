8a5cd7b2f0d2b0c203ed90ef1eaedfcebaa0ddc7
Colter Williams <cjwilliams@fullsail.edu>
2015-05-26 11:38:11 -0700

    finished first function
    function 1 finished

--- Williams_Colter_Functions_Assignment/js/script.js
+++ Williams_Colter_Functions_Assignment/js/script.js
@@ -10,7 +10,7 @@
 //alert('testing 1,2,3');
 
 //create welcome calculator alert
-alert("Welcome to the body calculator. This calculator will help you calculate your percent of weight change. ");
+alert("Welcome to the body calculator. This calculator will help you calculate how many pounds you have added/subtracted and then tell you what that percentage is. ");
 
 //create variable for old weight (pounds)
 var oldWeight = prompt("Enter old weight in pounds");
@@ -18,10 +18,18 @@ var oldWeight = prompt("Enter old weight in pounds");
 //create variable for current weight (pounds)
 var currentWeight = prompt("Enter current weight in pounds");
 
+
 //percent is 100 variable
 var percent100 = 100;
 
 //loading screen for fun
 alert("Processing information!One moment Please.");
 
-//create function for calculatre percent
+//create function variable for weight
+var resultWeight = calculatePercent(currentWeight,oldWeight);
+console.log("Your weight change is " +resultWeight +" pounds more or less.");
+
+//create first function
+function calculatePercent(c,o,){
+      return c - o;
+}
