39a7e370025e8db23f9c192837293695476c73ba
Colter Williams <cjwilliams@fullsail.edu>
2015-05-26 10:38:17 -0700

    created files for this weeks assignment

---
+++ Williams_Colter_Functions_Assignment/js/script.js
@@ -0,0 +1,13 @@
+/*/**
+ * Created by Colter Williams
+ * 5/26/15
+ * Section 01
+ * Williams_Colter_Functions_Assignment
+ */
+
+
+//created test to see if html is connected
+//alert('testing 1,2,3');
+
+//create welcome calculator alert
+alert("Welcome to the body calculator. This calculator will help you keep track of how much weight you have gained ");
