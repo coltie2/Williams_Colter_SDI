748e25f77928c4a37bade1cd7cacb4560f120176
Colter Williams <cjwilliams@fullsail.edu>
2015-05-26 11:09:55 -0700

    created variables to start first function
    created all the variables needed for the first function

--- Williams_Colter_Functions_Assignment/js/script.js
+++ Williams_Colter_Functions_Assignment/js/script.js
@@ -10,4 +10,18 @@
 //alert('testing 1,2,3');
 
 //create welcome calculator alert
-alert("Welcome to the body calculator. This calculator will help you keep track of how much weight you have gained ");
+alert("Welcome to the body calculator. This calculator will help you calculate your percent of weight change. ");
+
+//create variable for old weight (pounds)
+var oldWeight = prompt("Enter old weight in pounds");
+
+//create variable for current weight (pounds)
+var currentWeight = prompt("Enter current weight in pounds");
+
+//percent is 100 variable
+var percent100 = 100;
+
+//loading screen for fun
+alert("Processing information!One moment Please.");
+
+//create function for calculatre percent
