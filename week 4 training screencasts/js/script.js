e0f9295a5bd228491897464f38f9f768f6ad524a
Colter Williams <cjwilliams@fullsail.edu>
2015-05-25 17:19:47 -0700

    loops!

--- week 4 training screencasts/js/script.js
+++ week 4 training screencasts/js/script.js
@@ -77,7 +77,7 @@ function calcAreaP(width, height){
 
 //functions - anonymous functions
 
-var calcArea = function(width, height){ //defining
+/*var calcArea = function(width, height){ //defining
     //code the function runs
     var area = width * height;
     return area;
@@ -85,3 +85,32 @@ var calcArea = function(width, height){ //defining
 var a = calcArea(20, 30); //invoking
 
 console.log(a);
+*/
+
+//console.log(looooooooooops)
+
+/* var b = 10; //sets the index
+
+while (b > 0){ //sets the condition
+    console.log(b + " kegs on the wall");
+    b--; // increments or decrements the index
+}
+*/
+
+//do while loops
+
+/* var c = 10;
+
+do{
+    console.log(c + " kegs on the wall");
+    c--;
+}
+while(c > 0);
+*/
+
+
+//for poops
+
+for (var i = 10; i > 0; i--){
+    console.log(i + " kegs on the wall");
+}
